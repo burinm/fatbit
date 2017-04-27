ce703c4794d6f48b41a1713e2b450326d37af2c6
burin <Micheal.Burin@colorado.edu>
2017-04-27 12:56:35 -0600

    Connection Interval coded and documented

--- atmel/src/service.h
+++ atmel/src/service.h
@@ -3,20 +3,43 @@
 
 #include "at_ble_api.h"
 
-#define SERVICE_ADVERTISING_INTERVAL    1000    // .625 * 1000 = 625ms
+#define SERVICE_ADVERTISING_INTERVAL    1000    // .625ms * 1000 = 625ms
 #define SERVICE_ADVERTISING_TIMEOUT     0       // Disable adverftisement timeout
 
 #define SERVICE_EVENT_LOOP_TIMEOUT      100     // milliseconds, controls how often to wake up 
                                                 //  and check the UART message Q
 
 // 7.5ms <-> 4 seconds
-#define SERVICE_CONN_INT_MIN            1000
-#define SERVICE_CONN_INT_MAX            1000    // 1.25ms * 600 = 750ms
+/* The sensor array only sends updates of sunlight and pulse every 3.75 seconds
+    
+    So, in reality, the connection interval only needs to be less than 3.75 seconds.
+*/
 
-#define SERVICE_CONN_LAT                0      // Number of events
-#define SERVICE_CONN_SUP_TIMOUT         300    // 10ms * 300 = 3s
-#define SERVICE_CONN_CE_MIN             3200
-#define SERVICE_CONN_CE_MAX             3200   // .625ms * 3200 = 2 seconds
+#if 1
+#define SERVICE_CONN_INT_MIN           200   
+#define SERVICE_CONN_INT_MAX           240   // 1.25ms * 240 = 300ms 
+#define SERVICE_CONN_LAT               4     // 300ms * (4 + 1) = 1.5 second 
+#define SERVICE_CONN_SUP_TIMOUT        330   // 10ms * 330 = 3.3s , Need 1.5s + (6 * 300ms) = 3.3
+#define SERVICE_CONN_CE_MIN            0     // Not used
+#define SERVICE_CONN_CE_MAX            0     // Not used
+#endif
+
+
+/*
+    If we were to update the heart rate monitor every pulse (instead of sending and
+    average), we would need to update at this rate:
+
+    Fastest pulse = 220BPM = 3.7BPS = 272ms (this would be the connection interval max)
+*/
+
+#if 0 
+#define SERVICE_CONN_INT_MIN           200 
+#define SERVICE_CONN_INT_MAX           217    // 1.25ms * 272 = 271ms 
+#define SERVICE_CONN_LAT               3      // 271ms * (1+3) = 1 second
+#define SERVICE_CONN_SUP_TIMOUT        260    // 10ms * 260 = 2.6s , Need 1s + (271ms * 6) = 2.6s 
+#define SERVICE_CONN_CE_MIN            0      // Not used
+#define SERVICE_CONN_CE_MAX            0      // Not used
+#endif
 
 extern at_ble_handle_t master_connection_handle;
 
