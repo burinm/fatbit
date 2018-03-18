fa91e1016e290f8d3e1868f5081145928b1bde25
burin <21989631+burinm@users.noreply.github.com>
2018-03-17 19:59:07 -0600

    Removing cruft

--- extras/frequency.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Find the frequency of letters in prose
-for i in {a..z}; do echo -n "$i "; fgrep -i -o $i $1 | wc -l; done | sort -r -n -k 2
--- extras/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf caesar_cipher
-gcc caesar_cipher.c -o caesar_cipher
-
-rm -rf diffie-hellman
-gcc diffie-hellman.c -o diffie-hellman
